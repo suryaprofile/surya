db6121a24a345cf8e58078aa13b67669c0b0b812
surya8332929773 <75784177+surya8332929773@users.noreply.github.com>
2021-05-01 15:11:52 +0530

    site response

---
+++ js/js.js
@@ -0,0 +1,311 @@
+// JavaScript Document
+
+
+!(function ($) {
+    "use strict";
+
+    // Hero typed
+    if ($('.typed').length) {
+        var typed_strings = $(".typed").data('typed-items');
+        typed_strings = typed_strings.split(',')
+        new Typed('.typed', {
+            strings: typed_strings,
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+
+    // Smooth scroll for the navigation menu and links with .scrollto classes
+    $(document).on('click', '.nav-menu a, .scrollto', function (e) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            e.preventDefault();
+            var target = $(this.hash);
+            if (target.length) {
+
+                var scrollto = target.offset().top;
+
+                $('html, body').animate({
+                    scrollTop: scrollto
+                }, 1500, 'easeInOutExpo');
+
+                if ($(this).parents('.nav-menu, .mobile-nav').length) {
+                    $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+                    $(this).closest('li').addClass('active');
+                }
+
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+                }
+                return false;
+            }
+        }
+    });
+
+    // Activate smooth scroll on page load with hash links in the url
+    $(document).ready(function () {
+
+    });
+
+    $(document).on('click', '.mobile-nav-toggle', function (e) {
+        $('body').toggleClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+    });
+
+    $(document).click(function (e) {
+        var container = $(".mobile-nav-toggle");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            if ($('body').hasClass('mobile-nav-active')) {
+                $('body').removeClass('mobile-nav-active');
+                $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            }
+        }
+    });
+
+    // Navigation active state on scroll
+    var nav_sections = $('section');
+    var main_nav = $('.nav-menu, .mobile-nav');
+
+    $(window).on('scroll', function () {
+        var cur_pos = $(this).scrollTop() + 100;
+
+        nav_sections.each(function () {
+            var top = $(this).offset().top,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                if (cur_pos <= bottom) {
+                    main_nav.find('li').removeClass('active');
+                }
+                main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+            }
+            if (cur_pos < 200) {
+                $(".nav-menu ul:first li:first").addClass('active');
+            }
+        });
+    });
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    $('.back-to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    // jQuery counterUp
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    // Skills section
+    $('.skills-content').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {
+        offset: '50%'
+    });
+
+    // Porfolio isotope and filter
+    $(window).on('load', function () {
+        var portfolioIsotope = $('.portfolio-container').isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
+
+        $('#portfolio-flters li').on('click', function () {
+            $("#portfolio-flters li").removeClass('filter-active');
+            $(this).addClass('filter-active');
+
+            portfolioIsotope.isotope({
+                filter: $(this).data('filter')
+            });
+            aos_init();
+        });
+
+        // Initiate venobox (lightbox feature used in portofilo)
+        $(document).ready(function () {
+            $('.venobox').venobox();
+        });
+    });
+
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".owl-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            900: {
+                items: 3
+            }
+        }
+    });
+
+
+    $(".btm").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1200);
+    });
+
+
+    // Portfolio details carousel
+    $(".portfolio-details-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+    //
+    //  // Init AOS
+    //  function aos_init() {
+    //    aos_init.init({
+    //      
+    //     /* easing: "ease-in-out-back",*/
+    //       delay:"0",
+    //	    duration:"1000",
+    //    });
+    //  }
+    //  $(window).on('load', function() {
+    //    aos_init();
+    //  });
+
+})(jQuery); // JavaScript Document
+
+jQuery(document).ready(function ($) {
+    //define store some initial variables
+    var halfWindowH = $(window).height() * 0.5,
+        halfWindowW = $(window).width() * 0.5,
+        //define a max rotation value (X and Y axises)
+        maxRotationY = 5,
+        maxRotationX = 3,
+        aspectRatio;
+
+    //detect if hero <img> has been loaded and evaluate its aspect-ratio
+    $('.cd-floating-background').find('img').eq(0).load(function () {
+        aspectRatio = $(this).width() / $(this).height();
+        if ($('html').hasClass('preserve-3d')) initBackground();
+    }).each(function () {
+        //check if image was previously load - if yes, trigger load event
+        if (this.complete) $(this).load();
+    });
+
+    //detect mouse movement
+    $('.cd-background-wrapper').each(function () {
+        $(this).on('mousemove', function (event) {
+            var wrapperOffsetTop = $(this).offset().top;
+            if ($('html').hasClass('preserve-3d')) {
+                window.requestAnimationFrame(function () {
+                    moveBackground(event, wrapperOffsetTop);
+                });
+            }
+        });
+    });
+
+    //on resize - adjust .cd-background-wrapper and .cd-floating-background dimentions and position
+    $(window).on('resize', function () {
+        if ($('html').hasClass('preserve-3d')) {
+            window.requestAnimationFrame(function () {
+                halfWindowH = $(window).height() * 0.5,
+                    halfWindowW = $(window).width() * 0.5;
+                initBackground();
+            });
+        } else {
+            $('.cd-background-wrapper').attr('style', '');
+            $('.cd-floating-background').attr('style', '').removeClass('is-absolute');
+        }
+    });
+
+    function initBackground() {
+        var wrapperHeight = Math.ceil(halfWindowW * 2 / aspectRatio),
+            proportions = (maxRotationY > maxRotationX) ? 1.1 / (Math.sin(Math.PI / 2 - maxRotationY * Math.PI / 180)) : 1.1 / (Math.sin(Math.PI / 2 - maxRotationX * Math.PI / 180)),
+            newImageWidth = Math.ceil(halfWindowW * 2 * proportions),
+            newImageHeight = Math.ceil(newImageWidth / aspectRatio),
+            newLeft = halfWindowW - newImageWidth / 2,
+            newTop = (wrapperHeight - newImageHeight) / 2;
+
+        //set an height for the .cd-background-wrapper
+        $('.cd-background-wrapper').css({
+            'height': wrapperHeight,
+        });
+        //set dimentions and position of the .cd-background-wrapper		
+        $('.cd-floating-background').addClass('is-absolute').css({
+            'left': newLeft,
+            'top': newTop,
+            'width': newImageWidth,
+        });
+    }
+
+    function moveBackground(event, topOffset) {
+        var rotateY = ((-event.pageX + halfWindowW) / halfWindowW) * maxRotationY,
+            yPosition = event.pageY - topOffset,
+            rotateX = ((yPosition - halfWindowH) / halfWindowH) * maxRotationX;
+
+        if (rotateY > maxRotationY) rotateY = maxRotationY;
+        if (rotateY < -maxRotationY) rotateY = -maxRotationY;
+        if (rotateX > maxRotationX) rotateX = maxRotationX;
+        if (rotateX < -maxRotationX) rotateX = -maxRotationX;
+
+        $('.cd-floating-background').css({
+            '-moz-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
+            '-webkit-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
+            '-ms-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
+            '-o-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
+            'transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
+        });
+    }
+});
+
+/* 	Detect "transform-style: preserve-3d" support, or update csstransforms3d for IE10 ? #762
+	https://github.com/Modernizr/Modernizr/issues/762 */
+(function getPerspective() {
+    var element = document.createElement('p'),
+        html = document.getElementsByTagName('html')[0],
+        body = document.getElementsByTagName('body')[0],
+        propertys = {
+            'webkitTransformStyle': '-webkit-transform-style',
+            'MozTransformStyle': '-moz-transform-style',
+            'msTransformStyle': '-ms-transform-style',
+            'transformStyle': 'transform-style'
+        };
+
+    body.insertBefore(element, null);
+
+    for (var i in propertys) {
+        if (element.style[i] !== undefined) {
+            element.style[i] = "preserve-3d";
+        }
+    }
+
+    var st = window.getComputedStyle(element, null),
+        transform = st.getPropertyValue("-webkit-transform-style")
+        || st.getPropertyValue("-moz-transform-style")
+        || st.getPropertyValue("-ms-transform-style")
+        || st.getPropertyValue("transform-style");
+
+    if (transform !== 'preserve-3d') {
+        html.className += ' no-preserve-3d';
+    } else {
+        html.className += ' preserve-3d';
+    }
+    document.body.removeChild(element);
+
+})();
