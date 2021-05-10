4815a19c1b65eaf926214f16de8e39efb59fd7a2
surya8332929773 <75784177+surya8332929773@users.noreply.github.com>
2021-05-10 12:28:08 +0530

    Update js.js

--- js/js.js
+++ js/js.js
@@ -623,4 +623,14 @@ jQuery(document).ready(function ($) {
       }
     }
   });
-
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out'
+//      once: true,
+//      mirror: false
+    })
+  });
